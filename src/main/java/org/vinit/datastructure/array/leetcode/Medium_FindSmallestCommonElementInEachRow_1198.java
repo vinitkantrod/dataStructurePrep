a19f8bc77b93f16780bbac99b69486b781032309
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-09 20:59:15 +0530

    solved arrays intersections

---
+++ src/main/java/org/vinit/datastructure/array/leetcode/Medium_FindSmallestCommonElementInEachRow_1198.java
@@ -0,0 +1,23 @@
+package org.vinit.datastructure.array.leetcode;
+
+import java.util.*;
+
+public class Medium_FindSmallestCommonElementInEachRow_1198 {
+
+    public int smallestCommonElement(int[][] mat) {
+        Set<Integer> mainSet = new HashSet<>();
+        if (mat.length == 0) return -1;
+        for (int i : mat[0]) mainSet.add(i);
+        if (mat.length == 1) return mat[0][0];
+        for (int i = 1; i < mat.length; i++) {
+            Set<Integer> temp = new HashSet<>();
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mainSet.contains(mat[i][j]))
+                    temp.add(mat[i][j]);
+            }
+            mainSet = temp;
+        }
+        SortedSet<Integer> sortedVersion = new TreeSet<>(mainSet);
+        return !sortedVersion.isEmpty() ? sortedVersion.first() : -1;
+    }
+}
