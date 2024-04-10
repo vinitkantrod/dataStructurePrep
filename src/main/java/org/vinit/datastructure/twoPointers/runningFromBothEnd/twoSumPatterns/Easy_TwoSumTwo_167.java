a99b58a93abd4bb75f9bf7e818ad8468f848e79e
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-10 10:44:29 +0530

    2 sum

---
+++ src/main/java/org/vinit/datastructure/twoPointers/runningFromBothEnd/twoSumPatterns/Easy_TwoSumTwo_167.java
@@ -0,0 +1,20 @@
+package org.vinit.datastructure.twoPointers.runningFromBothEnd.twoSumPatterns;
+
+public class Easy_TwoSumTwo_167 {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length - 1;
+        while (start < end) {
+            if (numbers[start] + numbers[end] == target) {
+                return new int[]{start + 1, end + 1};
+            }
+            if (numbers[start] + numbers[end] < target) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
