f3344e8784c881d23be923f08d503628244365ad
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-11 13:22:31 +0530

    solved easy string problem

---
+++ src/main/java/org/vinit/datastructure/strings/leetcode/Easy_RemoveDigitFromNumberToMaximiseResult_2259.java
@@ -0,0 +1,15 @@
+package org.vinit.datastructure.strings.leetcode;
+
+public class Easy_RemoveDigitFromNumberToMaximiseResult_2259 {
+
+    public String removeDigit(String number, char digit) {
+        int index = 0;
+        for (int i = 0; i < number.length(); i++) {
+            if (number.charAt(i) == digit) {
+                index = i;
+                if (i < number.length() - 1 && digit < number.charAt(i + 1)) break;;
+            }
+        }
+        return number.substring(0, index) + number.substring(index + 1);
+    }
+}
