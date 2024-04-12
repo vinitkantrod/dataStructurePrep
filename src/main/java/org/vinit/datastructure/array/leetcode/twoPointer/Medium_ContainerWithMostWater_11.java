33e8855f5d08ee39c16c08244c14e921eedc7035
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-12 15:33:56 +0530

    medium trapping rain water

--- src/main/java/org/vinit/datastructure/array/leetcode/twoPointer/Medium_ContainerWithMostWater_11.java
+++ src/main/java/org/vinit/datastructure/array/leetcode/twoPointer/Medium_ContainerWithMostWater_11.java
@@ -2,7 +2,27 @@ package org.vinit.datastructure.array.leetcode.twoPointer;
 
 public class Medium_ContainerWithMostWater_11 {
 
-    public int maxArea(int[] height) {
+    public static int maxArea(int[] height) {
+        int arrWidth = height.length - 1;
 
+        int left = 0;
+        int right = height.length - 1;
+        int maxWater = 0;
+        while (left < right) {
+            int leftMax = height[left];
+            int rightMax = height[right];
+            System.out.println(left + "- " + right);
+            int minHeight = Math.min(leftMax, rightMax);
+            maxWater = Math.max(maxWater, minHeight * arrWidth);
+            System.out.println(maxWater);
+            arrWidth--;
+            if (leftMax < rightMax) left++;
+            else right--;
+        }
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
     }
 }
