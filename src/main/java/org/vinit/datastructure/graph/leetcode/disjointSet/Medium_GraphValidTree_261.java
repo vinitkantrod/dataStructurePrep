802abdfde55868666203ab81cfb2d158c94bc510
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-12 23:34:45 +0530

    ways of solving graph problems

--- src/main/java/org/vinit/datastructure/graph/leetcode/disjointSet/Medium_GraphValidTree_261.java
+++ src/main/java/org/vinit/datastructure/graph/leetcode/disjointSet/Medium_GraphValidTree_261.java
@@ -2,6 +2,17 @@ package org.vinit.datastructure.graph.leetcode.disjointSet;
 
 public class Medium_GraphValidTree_261 {
 
+    /**
+     * Ways to solve it:
+     * Graph can be represented as adjancency list, adjacencyMatrix, linked Representation
+     * Iterative DFS - Adjacency List, stacks
+     * Recursive DFS
+     * Iterative BFS - queue, Adjacency List
+     * Union Find (BEST)
+     * @param n
+     * @param edges
+     * @return
+     */
     public boolean validTree(int n, int[][] edges) {
         return true;
     }
