2e56f33b0e43134108dc9a39dc60e23f99cd1426
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-13 14:03:21 +0530

    connectedComponent

--- src/main/java/org/vinit/datastructure/dp/leetcode/Hard_MaximalRectangle_85.java
+++ src/main/java/org/vinit/datastructure/dp/leetcode/Hard_MaximalRectangle_85.java
@@ -2,6 +2,23 @@ package org.vinit.datastructure.dp.leetcode;
 
 public class Hard_MaximalRectangle_85 {
     public int maximalRectangle(char[][] matrix) {
-        return -1;
+        // O(N*(N*M)) = TC: O(N^2*M)
+        // SC O(N*M)
+        if (matrix.length == 0) return 0;
+        int maxArea = 0;
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = j > 0 ? dp[i][j - 1] + 1 : 1;
+                    int width = dp[i][j];
+                    for (int k = i; k >= 0; k--) {
+                        width = Math.min(width, dp[k][j]);
+                        maxArea = Math.max(maxArea, width * (i - k + 1));
+                    }
+                }
+            }
+        }
+        return maxArea;
     }
 }
