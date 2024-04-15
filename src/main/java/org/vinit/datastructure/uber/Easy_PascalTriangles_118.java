8435c600354a77af0784bca8743ceaf103001cc3
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-15 16:06:07 +0530

    pascal triangles

---
+++ src/main/java/org/vinit/datastructure/uber/Easy_PascalTriangles_118.java
@@ -0,0 +1,26 @@
+package org.vinit.datastructure.uber;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Easy_PascalTriangles_118 {
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        result.get(0).add(1);
+        for (int numRow = 1; numRow < numRows; numRow++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = result.get(numRow - 1);
+            row.add(1);
+
+            for (int j = 1; j < numRow; j++) {
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+            row.add(1);
+            result.add(row);
+        }
+        return result;
+    }
+}
