f57fa0ff92626063a1e2d6100c9541bd96f4226e
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-15 16:47:46 +0530

    min max game

---
+++ src/main/java/org/vinit/datastructure/array/leetcode/Medium_FindTriangluarSumOfAnArray_2221.java
@@ -0,0 +1,23 @@
+package org.vinit.datastructure.array.leetcode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Medium_FindTriangluarSumOfAnArray_2221 {
+
+    public static int triangularSum(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        while (nums.length != 1) {
+            int[] num = new int[nums.length - 1];
+            for (int i = 0; i < num.length; i++) {
+                num[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+            nums = num;
+        }
+        return nums[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(triangularSum(new int[]{1,2,3,4,5,6,7}));
+    }
+}
