a700d8f6d6d93f77c397a96876dd80f754c7a2e0
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-16 11:10:05 +0530

    BST to circular double linked list

---
+++ src/main/java/org/vinit/datastructure/tree/leetcode/Medium_ConvertBinarySearchTreeToSortedDoubleCircularLinkedList_426.java
@@ -0,0 +1,45 @@
+package org.vinit.datastructure.tree.leetcode;
+
+public class Medium_ConvertBinarySearchTreeToSortedDoubleCircularLinkedList_426 {
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val,Node _left,Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    };
+    Node first = null;
+    Node last = null;
+    public Node treeToDoublyList(Node root) {
+        if (root == null) return null;
+        helper(root);
+        first.left = last;
+        last.right = first;
+        return first;
+    }
+    private void helper(Node root) {
+        if (root != null) {
+            helper(root.left);
+            if (last != null) {
+                last.right = root;
+                root.left = last;
+            } else {
+                first = root;
+            }
+            last = root;
+            helper(root.right);
+        }
+
+    }
+}
