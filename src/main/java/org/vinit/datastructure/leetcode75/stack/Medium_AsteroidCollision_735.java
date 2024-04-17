b8e1c7096a65abdd473df44bbf27de0334667fae
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-17 15:00:37 +0530

    asteroid collision

---
+++ src/main/java/org/vinit/datastructure/leetcode75/stack/Medium_AsteroidCollision_735.java
@@ -0,0 +1,48 @@
+package org.vinit.datastructure.leetcode75.stack;
+
+import java.util.Stack;
+
+public class Medium_AsteroidCollision_735 {
+
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+
+        for(int val : asteroids) {
+            if(st.isEmpty() || val > 0) {
+                st.push(val);
+            }
+            else {
+                while(true) {
+                    int peek = st.peek();
+                    if(peek < 0) {
+                        st.push(val);
+                        break;
+                    }
+                    else if(peek == -val) {
+                        st.pop();
+                        break;
+                    }
+                    else if(peek > -val) {
+                        break;
+                    }
+                    else {
+                        st.pop();
+                        if(st.isEmpty()) {
+                            st.push(val);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        int i = st.size();
+        int ans[] = new int[i];
+        while(!st.isEmpty()) {
+            if(i > 0)
+                ans[i-1] = st.pop();
+            i--;
+        }
+        return ans;
+    }
+}
