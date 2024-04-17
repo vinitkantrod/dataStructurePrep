a35f376ac5d7f9409a1fa6080e638b971cff90cf
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-17 14:42:29 +0530

    remove star from string

---
+++ src/main/java/org/vinit/datastructure/leetcode75/stack/Medium_RemovingStarFromString_2390.java
@@ -0,0 +1,23 @@
+package org.vinit.datastructure.leetcode75.stack;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Stack;
+
+public class Medium_RemovingStarFromString_2390 {
+
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '*') stack.pop();
+            else stack.push(ch);
+        }
+        System.out.println(stack);
+        String str = "";
+        while (!stack.isEmpty()) {
+            str = String.valueOf(stack.pop()).concat(str);
+        }
+        return str;
+    }
+}
