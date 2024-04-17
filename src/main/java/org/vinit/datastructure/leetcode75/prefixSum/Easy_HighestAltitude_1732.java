95db3273f618a260bae514990e59920cd7ea3860
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-17 13:09:03 +0530

    leetcode 75 highest altitude

---
+++ src/main/java/org/vinit/datastructure/leetcode75/prefixSum/Easy_HighestAltitude_1732.java
@@ -0,0 +1,15 @@
+package org.vinit.datastructure.leetcode75.prefixSum;
+
+public class Easy_HighestAltitude_1732 {
+
+    public int largestAltitude(int[] gain) {
+        int maxGain = 0;
+        int currGain = 0;
+        for (int i = 0; i < gain.length; i++) {
+            currGain = i == 0 ? gain[i] : currGain + gain[i];
+            System.out.println(currGain);
+            maxGain = Math.max(maxGain, currGain);
+        }
+        return maxGain;
+    }
+}
