bb494752d9e12345271e1430da5948e55bd3e1cd
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-18 11:11:06 +0530

    leetcode 75

---
+++ src/main/java/org/vinit/datastructure/leetcode75/binaryTreeDFS/Medium_LowestCommonAncestorOfTree_236.java
@@ -0,0 +1,27 @@
+package org.vinit.datastructure.leetcode75.binaryTreeDFS;
+
+public class Medium_LowestCommonAncestorOfTree_236 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (p == root || q == root) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        if (left != null) return left;
+        if (right != null) return right;
+        return null;
+    }
+}
