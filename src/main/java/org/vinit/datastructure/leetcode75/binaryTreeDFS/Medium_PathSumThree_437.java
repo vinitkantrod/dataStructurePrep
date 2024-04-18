45687a33a1cccf30e2a9c9ca8383b4dc4a1ba944
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-18 10:38:46 +0530

    leetcode 75 path sum three

--- src/main/java/org/vinit/datastructure/leetcode75/binaryTreeDFS/Medium_PathSumThree_437.java
+++ src/main/java/org/vinit/datastructure/leetcode75/binaryTreeDFS/Medium_PathSumThree_437.java
@@ -1,5 +1,8 @@
 package org.vinit.datastructure.leetcode75.binaryTreeDFS;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Medium_PathSumThree_437 {
 
     public class TreeNode {
@@ -15,8 +18,23 @@ public class Medium_PathSumThree_437 {
         }
     }
 
+    int k = 0;
+    int count = 0;
+    Map<Long, Integer> h = new HashMap<>();
     public int pathSum(TreeNode root, int targetSum) {
-        return 0;
+        k = targetSum;
+        preOrder(root, 0L);
+        return count;
+    }
+    private void preOrder(TreeNode node, Long currSum) {
+        if (node == null) return;
+        currSum += node.val;
+        if (currSum == k) count += 1;
+        count += h.getOrDefault(currSum - k, 0);
+        h.put(currSum, h.getOrDefault(currSum, 0) + 1);
+        preOrder(node.left, currSum);
+        preOrder(node.right, currSum);
+        h.put(currSum, h.get(currSum) - 1);
     }
 
 }
