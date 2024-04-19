debf62ce89397792944a0303e00d9f5d86f49309
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-19 15:37:09 +0530

    non overlapping interval

---
+++ src/main/java/org/vinit/datastructure/intervals/Medium_NonOverlappingInterval_435.java
@@ -0,0 +1,21 @@
+package org.vinit.datastructure.intervals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Medium_NonOverlappingInterval_435 {
+
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[1], b[1]));
+        int prev = 0; int count = 1;
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i][0] >= intervals[prev][1]) {
+                prev = i;
+                count++;
+            }
+        }
+        return intervals.length - count;
+    }
+}
