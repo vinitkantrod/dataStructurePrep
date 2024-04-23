2f0da4129e78b86d62fdfdf89d24b6b510433be7
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-23 20:49:35 +0530

    number of special char

---
+++ src/main/java/org/vinit/datastructure/strings/leetcode/Easy_CountSpecialChar_3120.java
@@ -0,0 +1,26 @@
+package org.vinit.datastructure.strings.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Easy_CountSpecialChar_3120 {
+
+    public int numberOfSpecialChars(String word) {
+        char[] ch = word.toCharArray();
+        int count = 0;
+        Map<Character, Set<Character>> map = new HashMap<>();
+        for (char c : ch) {
+            Character charValue = Character.toLowerCase(c);
+            if (!map.containsKey(charValue)) {
+                map.put(charValue, new HashSet<>());
+            }
+            map.get(charValue).add(c);
+        }
+        for (Map.Entry<Character, Set<Character>> m : map.entrySet()) {
+            if (m.getValue().size() == 2) count++;
+        }
+        return count;
+    }
+}
