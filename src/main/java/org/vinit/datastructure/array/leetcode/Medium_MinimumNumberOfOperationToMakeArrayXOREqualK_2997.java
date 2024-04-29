0d1c20ce04a20079d5181f3badfeccc53c23d7ea
vinit kantrod <kantrod.vinit@gmail.com>
2024-04-29 08:16:16 +0530

    29th april challenge

---
+++ src/main/java/org/vinit/datastructure/array/leetcode/Medium_MinimumNumberOfOperationToMakeArrayXOREqualK_2997.java
@@ -0,0 +1,12 @@
+package org.vinit.datastructure.array.leetcode;
+
+public class Medium_MinimumNumberOfOperationToMakeArrayXOREqualK_2997 {
+    public int minOperations(int[] nums, int k) {
+        int finalXor = 0;
+        for (int n : nums) {
+            finalXor = finalXor ^ n;
+        }
+
+        return Integer.bitCount(finalXor ^ k);
+    }
+}
