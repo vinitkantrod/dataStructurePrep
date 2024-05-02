93e42aff135f248e4e6b43f2207d925e3d82cc3d
vinit kantrod <kantrod.vinit@gmail.com>
2024-05-02 22:01:52 +0530

    all path from start to target

---
+++ src/main/java/org/vinit/datastructure/graph/leetcode/dfs/Medium_AllPathFromSourceToTarget_797.java
@@ -0,0 +1,49 @@
+package org.vinit.datastructure.graph.leetcode.dfs;
+
+import java.util.*;
+
+public class Medium_AllPathFromSourceToTarget_797 {
+
+    int target;
+    int[][] graph;
+    List<List<Integer>> result;
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        this.target = graph.length - 1;
+        this.graph = graph;
+        result = new ArrayList<>();
+        LinkedList<Integer> path = new LinkedList<>();
+        path.addLast(0);
+        backtrack(0, path);
+        return result;
+    }
+    private void backtrack(int curr, LinkedList<Integer> path) {
+        if (curr == target) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int neighbour : graph[curr]) {
+            path.addLast(neighbour);
+            backtrack(neighbour, path);
+            path.removeLast();
+        }
+    }
+
+    private static void dfs(Map<Integer, List<Integer>> graph, int curr, int target, Set<Integer> visited) {
+        if (curr == target) {
+            response.add(new ArrayList<>(visited));
+            return;
+        }
+        for (Integer neighbour : graph.get(curr)) {
+            if (visited.contains(neighbour)) continue;
+            visited.add(neighbour);
+            dfs(graph, neighbour, target, visited);
+            visited.remove(neighbour);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] g = new int[][]{{1,2}, {3}, {3}, {}};
+        List<List<Integer>> r = allPathsSourceTarget(g);
+        for (List<Integer> rr : r) System.out.println(Arrays.asList(rr));
+    }
+}
