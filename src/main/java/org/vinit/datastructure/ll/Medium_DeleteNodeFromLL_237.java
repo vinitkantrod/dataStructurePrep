7fd3d82689d9357700cc05608512bf439c840be9
vinit kantrod <kantrod.vinit@gmail.com>
2024-05-06 20:20:47 +0530

    6th may challenge

--- src/main/java/org/vinit/datastructure/ll/Medium_DeleteNodeFromLL_237.java
+++ src/main/java/org/vinit/datastructure/ll/Medium_DeleteNodeFromLL_237.java
@@ -1,2 +1,13 @@
-package org.vinit.datastructure.ll;public class Medium_DeleteNodeFromLL_237 {
+package org.vinit.datastructure.ll;
+
+public class Medium_DeleteNodeFromLL_237 {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+    }
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
 }
---
+++ src/main/java/org/vinit/datastructure/ll/Medium_RemoveNodesFromLL_2487.java
@@ -0,0 +1,32 @@
+package org.vinit.datastructure.ll;
+
+import java.util.Stack;
+
+public class Medium_RemoveNodesFromLL_2487 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode removeNodes(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode curr = head;
+        while (curr != null) {
+            int currData = curr.val;
+            while (!stack.isEmpty() && stack.peek().val < currData) {
+                stack.pop();
+            }
+            stack.push(curr);
+            curr = curr.next;
+        }
+        head = null;
+        while (!stack.isEmpty()) {
+            ListNode top = stack.pop();
+            top.next = head;
+            head = top;
+        }
+        return head;
+    }
+}
