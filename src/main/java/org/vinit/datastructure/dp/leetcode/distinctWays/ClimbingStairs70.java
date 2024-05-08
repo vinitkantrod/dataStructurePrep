e55fac138c41fb0995143f17b6f22eed42bbd1a8
vinit kantrod <kantrod.vinit@gmail.com>
2024-05-08 19:53:04 +0530

    climbing stairs

---
+++ src/main/java/org/vinit/datastructure/dp/leetcode/distinctWays/ClimbingStairs70.java
@@ -0,0 +1,22 @@
+package org.vinit.datastructure.dp.leetcode.distinctWays;
+
+public class ClimbingStairs70 {
+
+    public static int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int a = 1, b = 2;
+        for (int i = 3; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(2));
+        System.out.println(climbStairs(3));
+        System.out.println(climbStairs(45));
+    }
+}
