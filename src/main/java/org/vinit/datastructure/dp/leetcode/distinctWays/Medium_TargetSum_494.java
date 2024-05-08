45a8c79d1901127301eaa96e776c2be1ab773378
vinit kantrod <kantrod.vinit@gmail.com>
2024-05-08 21:34:46 +0530

    Target Sum

---
+++ src/main/java/org/vinit/datastructure/dp/leetcode/distinctWays/Medium_TargetSum_494.java
@@ -0,0 +1,25 @@
+package org.vinit.datastructure.dp.leetcode.distinctWays;
+
+import java.util.Arrays;
+
+public class Medium_TargetSum_494 {
+
+    int total = 0;
+    public int findTargetSumWays(int[] nums, int target) {
+        total = Arrays.stream(nums).sum();
+        int[][] memo = new int[nums.length][2 * total + 1];
+        for (int[] x : memo) Arrays.fill(x, Integer.MIN_VALUE);
+        return calculate(nums, 0, 0, target, memo);
+    }
+    private int calculate(int[] nums, int i, int sum, int target, int[][] memo) {
+        if (i == nums.length) {
+            if (sum == target) return 1;
+            else return 0;
+        }
+        if (memo[i][sum + total] != Integer.MIN_VALUE) return memo[i][sum + total];
+        int add = calculate(nums, i + 1, sum + nums[i], target, memo);
+        int sub = calculate(nums, i  + 1, sum - nums[i], target, memo);
+        memo[i][sum + total] = add + sub;
+        return memo[i][sum + total];
+    }
+}
