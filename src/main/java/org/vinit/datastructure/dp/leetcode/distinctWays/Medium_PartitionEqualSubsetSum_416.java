2e6f961c4ac7f92d0f716c9aafc24ae540cf73ba
vinit kantrod <kantrod.vinit@gmail.com>
2024-05-08 20:40:46 +0530

    partition equal subset sum

---
+++ src/main/java/org/vinit/datastructure/dp/leetcode/distinctWays/Medium_PartitionEqualSubsetSum_416.java
@@ -0,0 +1,28 @@
+package org.vinit.datastructure.dp.leetcode.distinctWays;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Medium_PartitionEqualSubsetSum_416 {
+
+    public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        for (int i : nums) totalSum += i;
+        if (totalSum % 2 != 0) return false;
+        int subSetSum = totalSum / 2;
+        int N = nums.length;
+        boolean[][] dp = new boolean[N + 1][subSetSum + 1];
+        dp[0][0] = true;
+        for (int i = 1; i < N + 1; i++) {
+            int curr = nums[i - 1];
+            for (int j = 0; j <= subSetSum; j++) {
+                if (j < curr) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - curr];
+                }
+            }
+        }
+        return dp[N][subSetSum];
+    }
+}
