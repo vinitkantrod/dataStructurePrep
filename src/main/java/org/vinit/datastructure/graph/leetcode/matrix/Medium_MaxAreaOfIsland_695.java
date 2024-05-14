fe78331c55c4e3fc1bf1a996a3789390a2b60f7c
vinit kantrod <kantrod.vinit@gmail.com>
2024-05-14 21:33:22 +0530

    Max area of island

---
+++ src/main/java/org/vinit/datastructure/graph/leetcode/matrix/Medium_MaxAreaOfIsland_695.java
@@ -0,0 +1,30 @@
+package org.vinit.datastructure.graph.leetcode.matrix;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Medium_MaxAreaOfIsland_695 {
+    public int maxAreaOfIsland(int[][] grid) {
+        this.grid = grid;
+        seen = new boolean[grid.length][grid[0].length];
+        int ans = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                ans = Math.max(ans, area(r, c));
+            }
+        }
+        return ans;
+    }
+
+    int[][] grid;
+    boolean[][] seen;
+
+    public int area(int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length ||
+                seen[r][c] || grid[r][c] == 0)
+            return 0;
+        seen[r][c] = true;
+        return (1 + area(r+1, c) + area(r-1, c)
+                + area(r, c-1) + area(r, c+1));
+    }
+}
