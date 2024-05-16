a6a953f4e7134fd6db9fdd7662479ec1f92f5643
vinit kantrod <kantrod.vinit@gmail.com>
2024-05-16 15:56:28 +0530

    check equivalence

---
+++ src/main/java/org/vinit/datastructure/tree/leetcode/Medium_CheckIfTwoExpressionTreeAreEquivalent_1612.java
@@ -0,0 +1,37 @@
+package org.vinit.datastructure.tree.leetcode;
+
+public class Medium_CheckIfTwoExpressionTreeAreEquivalent_1612 {
+
+    public class Node {
+        char val;
+        Node left;
+        Node right;
+        Node() {}
+        Node(char val) { this.val = val; }
+        Node(char val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean checkEquivalence(Node root1, Node root2) {
+        int[] map = new int[26];
+        addToMap(root1, map, 1);
+        addToMap(root2, map, -1);
+        for (int i = 0; i < 26; i++) {
+            if (map[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private void addToMap(Node root, int[] map, int value) {
+        if (root == null) return;
+        if (root.val <= 'z' && root.val >= 'a') {
+            map[root.val - 'a'] += value;
+        }
+        addToMap(root.left, map, value);
+        addToMap(root.right, map, value);
+    }
+}
