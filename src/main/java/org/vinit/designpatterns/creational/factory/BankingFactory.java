e357e250c670d21b2edf5a573e1b3ac5199590bc
vinit kantrod <kantrod.vinit@gmail.com>
2024-05-23 11:05:04 +0530

    banking factory methods

--- src/main/java/org/vinit/designpatterns/creational/factory/BankingFactory.java
+++ src/main/java/org/vinit/designpatterns/creational/factory/BankingFactory.java
@@ -1,2 +1,22 @@
-package org.vinit.designpatterns.creational.factory;public class BankingFactory {
+package org.vinit.designpatterns.creational.factory;
+
+public class BankingFactory {
+
+    public static BankAccount createAccount(String type) {
+        BankAccount bankAccount = null;
+        switch (type) {
+            case "P" -> {
+                bankAccount = new PersonalAccount(8.5D, 1_000_567L);
+                break;
+            }
+            case "B" -> {
+                bankAccount = new BusinessAccount(7.0D, 2_000_567L);
+                break;
+            }
+        }
+//        System.out.println(bankAccount.getLimitToAccountBalance());
+//        System.out.println(bankAccount.getAccountNumber());
+//        System.out.println(bankAccount.getInterestRate());
+        return bankAccount;
+    }
 }
--- src/main/java/org/vinit/designpatterns/creational/factory/BankingMain.java
+++ src/main/java/org/vinit/designpatterns/creational/factory/BankingMain.java
@@ -1,8 +1,18 @@
 package org.vinit.designpatterns.creational.factory;
 
+import java.util.Scanner;
+
 public class BankingMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to ICICI Bank");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter\n" +
+                " P for Personal account\n" +
+                " B for Business account\n" +
+                "----------------------------");
+        String type = in.next();
+        BankAccount acc = BankingFactory.createAccount(type.toUpperCase());
+        System.out.println(acc.getInterestRate() + ", " + acc.getLimitToAccountBalance());
     }
 }
