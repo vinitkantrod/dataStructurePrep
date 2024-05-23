ab36ea6b3118997e8ad952dad44de41695596559
vinit kantrod <kantrod.vinit@gmail.com>
2024-05-23 13:24:07 +0530

    stair climbing dp

--- src/main/java/org/vinit/datastructure/leetcode/leetcode75/dp1D/Easy_MinCostClimbingStair_746.java
+++ src/main/java/org/vinit/datastructure/leetcode/leetcode75/dp1D/Easy_MinCostClimbingStair_746.java
@@ -6,22 +6,23 @@ import java.util.Arrays;
 public class Easy_MinCostClimbingStair_746 {
 
     public static int minCostClimbingStairs(int[] cost) {
-//        int[] dp = new int[cost.length + 1];
-//        for (int i = 2; i < cost.length; i++) dp[i] = Math.min(dp[i - 2] + cost[i], dp[i - 1] + cost[i - 1]);
-//        return dp[cost.length - 1];
-
-        int dp[] = new int[cost.length + 1];
-
-        // Start iteration from step 2, since the minimum cost of reaching
-        // step 0 and step 1 is 0
-        for (int i = 2; i < dp.length; i++) {
-            int takeOneStep = dp[i - 1] + cost[i - 1];
-            int takeTwoSteps = dp[i - 2] + cost[i - 2];
-            dp[i] = Math.min(takeOneStep, takeTwoSteps);
-        }
-
-        // The final element in minimumCost refers to the top floor
+        int[] dp = new int[cost.length + 1];
+        for (int i = 2; i < dp.length; i++)
+            dp[i] = Math.min(dp[i - 2] + cost[i-2], dp[i - 1] + cost[i - 1]);
         return dp[dp.length - 1];
+
+//        int dp[] = new int[cost.length + 1];
+//
+//        // Start iteration from step 2, since the minimum cost of reaching
+//        // step 0 and step 1 is 0
+//        for (int i = 2; i < dp.length; i++) {
+//            int takeOneStep = dp[i - 1] + cost[i - 1];
+//            int takeTwoSteps = dp[i - 2] + cost[i - 2];
+//            dp[i] = Math.min(takeOneStep, takeTwoSteps);
+//        }
+//
+//        // The final element in minimumCost refers to the top floor
+//        return dp[dp.length - 1];
     }
 
     public static void main(String[] args) {
