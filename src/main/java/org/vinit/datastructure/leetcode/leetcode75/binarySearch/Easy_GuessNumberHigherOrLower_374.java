738c2c75bc8bf08748b3de1b2fbd1b8e4e2d7d71
vinit kantrod <kantrod.vinit@gmail.com>
2024-06-07 12:59:37 +0530

    guess number higher or lower

---
+++ src/main/java/org/vinit/datastructure/leetcode/leetcode75/binarySearch/Easy_GuessNumberHigherOrLower_374.java
@@ -0,0 +1,23 @@
+package org.vinit.datastructure.leetcode.leetcode75.binarySearch;
+
+public class Easy_GuessNumberHigherOrLower_374 {
+
+    public int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int res = guess(mid);
+            if (res == 0)
+                return mid;
+            else if (res < 0)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+    int guess(int n) {
+        return n;
+    }
+}
