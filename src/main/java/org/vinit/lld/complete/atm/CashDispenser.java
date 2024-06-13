0a5a6c6723360e6cc88c02cc964e7fe077d705f7
vinit kantrod <kantrod.vinit@gmail.com>
2024-06-13 18:33:41 +0530

    added cash dispenser

---
+++ src/main/java/org/vinit/lld/complete/atm/CashDispenser.java
@@ -0,0 +1,15 @@
+package org.vinit.lld.complete.atm;
+
+public class CashDispenser {
+    private Double cashAvailable;
+    public CashDispenser(Double initialBal) {
+        this.cashAvailable = initialBal;
+    }
+    public synchronized void dispenseCash(Double amount) {
+        if (cashAvailable < amount) {
+            throw new IllegalArgumentException("Not enough cash available in the ATM.");
+        }
+        cashAvailable -= amount;
+        System.out.println("Cash Dispensed: " + amount);
+    }
+}
