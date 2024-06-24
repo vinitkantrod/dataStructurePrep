c30275f8aaf17b630bfcd35778e48c343536f611
vinit kantrod <kantrod.vinit@gmail.com>
2024-06-24 21:27:06 +0530

    valid palindrome

---
+++ src/main/java/org/vinit/datastructure/leetcode/leetcode150/arrays/Hard_TextJustification_68.java
@@ -0,0 +1,10 @@
+package org.vinit.datastructure.leetcode.leetcode150.arrays;
+
+import java.util.List;
+
+public class Hard_TextJustification_68 {
+
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        return null;
+    }
+}
