5baeebda9af00a8c5f4365c5c85bf7fd0be449f3
vinit kantrod <kantrod.vinit@gmail.com>
2024-06-24 21:38:39 +0530

    two number sum

---
+++ src/main/java/org/vinit/datastructure/leetcode/leetcode150/twoPointers/Medium_TwoSumTwo_167.java
@@ -0,0 +1,18 @@
+package org.vinit.datastructure.leetcode.leetcode150.twoPointers;
+
+public class Medium_TwoSumTwo_167 {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0, end = numbers.length - 1;
+        while (start < end) {
+            if (numbers[start] + numbers[end] == target) {
+                return new int[]{start + 1, end + 1};
+            } else if (numbers[start] + numbers[end] > target) {
+                end--;
+            } else {
+                start++;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
