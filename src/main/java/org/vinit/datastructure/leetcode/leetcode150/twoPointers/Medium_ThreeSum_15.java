47b47645b39f645575c1254702146a1036c75d8d
vinit kantrod <kantrod.vinit@gmail.com>
2024-06-24 22:14:48 +0530

    add 3 sum

---
+++ src/main/java/org/vinit/datastructure/leetcode/leetcode150/twoPointers/Medium_ThreeSum_15.java
@@ -0,0 +1,32 @@
+package org.vinit.datastructure.leetcode.leetcode150.twoPointers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Medium_ThreeSum_15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i+ 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    result.add(Arrays.asList(i, j, k));
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return result;
+    }
+}
