62b1fd08792d1885afa3ae35bf90d95ad9055b90
vinit kantrod <kantrod.vinit@gmail.com>
2024-06-24 09:03:50 +0530

    find index of substring

---
+++ src/main/java/org/vinit/datastructure/leetcode/leetcode150/arrays/Easy_FindIndexOfFirstOccuranceInString_28.java
@@ -0,0 +1,7 @@
+package org.vinit.datastructure.leetcode.leetcode150.arrays;
+
+public class Easy_FindIndexOfFirstOccuranceInString_28 {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
