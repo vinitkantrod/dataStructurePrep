d00168f8e0381fcc54509e7fa676427640b3c112
vinit kantrod <kantrod.vinit@gmail.com>
2024-06-26 21:24:43 +0530

    meeting OCroom two

---
+++ src/main/java/org/vinit/datastructure/leetcode/leetcode150/intervals/Medium_MeetingRoomsTwo_253.java
@@ -0,0 +1,42 @@
+package org.vinit.datastructure.leetcode.leetcode150.intervals;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Medium_MeetingRoomsTwo_253 {
+
+    public int minMeetingRooms(int[][] intervals) {
+        // Min heap
+        PriorityQueue<Integer> allocator =
+                new PriorityQueue<Integer>(
+                        intervals.length,
+                        new Comparator<Integer>() {
+                            public int compare(Integer a, Integer b) {
+                                return a - b;
+                            }
+                        });
+
+        // Sort the intervals by start time
+        Arrays.sort(
+                intervals,
+                new Comparator<int[]>() {
+                    public int compare(final int[] a, final int[] b) {
+                        return a[0] - b[0];
+                    }
+                });
+
+        // Add the first meeting
+        allocator.add(intervals[0][1]);
+
+        // Iterate over remaining intervals
+        for (int i = 1; i < intervals.length; i++) {
+            // If the room due to free up the earliest is free, assign that room to this meeting.
+            if (intervals[i][0] >= allocator.peek()) {
+                allocator.poll();
+            }
+            allocator.add(intervals[i][1]);
+        }
+        return allocator.size();
+    }
+}
