63b1bda82d790da01ee3b2814955d85696b14150
vinit kantrod <kantrod.vinit@gmail.com>
2024-06-28 01:20:16 +0530

    reverse ll 2

---
+++ src/main/java/org/vinit/datastructure/leetcode/leetcode150/linkedlist/Medium_ReverseLLTwo_92.java
@@ -0,0 +1,51 @@
+package org.vinit.datastructure.leetcode.leetcode150.linkedlist;
+
+public class Medium_ReverseLLTwo_92 {
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // Empty list
+        if (head == null) {
+            return null;
+        }
+        int m = left, n = right;
+        // Move the two pointers until they reach the proper starting point
+        // in the list.
+        ListNode cur = head, prev = null;
+        while (m > 1) {
+            prev = cur;
+            cur = cur.next;
+            m--;
+            n--;
+        }
+
+        // The two pointers that will fix the final connections.
+        ListNode con = prev, tail = cur;
+
+        // Iteratively reverse the nodes until n becomes 0.
+        ListNode third = null;
+        while (n > 0) {
+            third = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = third;
+            n--;
+        }
+
+        // Adjust the final connections as explained in the algorithm
+        if (con != null) {
+            con.next = prev;
+        } else {
+            head = prev;
+        }
+
+        tail.next = cur;
+        return head;
+    }
+}
